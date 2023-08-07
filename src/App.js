a4817fcc150316d759c121f7540e6346254f5a04
William Gislason <8203830+gislawill@users.noreply.github.com>
2023-08-06 21:39:56 -0700

    updating core packages, adding staging authentication screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import Map from './Map'
+import { useState } from 'react';
+import { StagingLogin } from 'staging/auth';
 
 function App() {
+  const [authed, setAuthed] = useState(false);
+
   return (
     <div className="App">
-      <Map />
+      {process.env.REACT_APP_USE_SITE_GATING === 'true' && !authed ? (
+        <StagingLogin setAuthed={setAuthed} />
+      ) : (
+        <Map />
+      )}
     </div>
   );
 }
