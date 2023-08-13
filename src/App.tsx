b831097104f7625124a74b3a611a1b9bd2c5f5c5
William Gislason <8203830+gislawill@users.noreply.github.com>
2023-08-13 14:02:35 -0700

    adding optional typescript

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Map from "./Map";
+import { useState } from "react";
+import { StagingLogin } from "./staging/auth";
+
+function App() {
+  const [authed, setAuthed] = useState(false);
+
+  return (
+    <div className="App">
+      {process.env.REACT_APP_USE_SITE_GATING === "true" && !authed ? (
+        <StagingLogin setAuthed={setAuthed} />
+      ) : (
+        <Map />
+      )}
+    </div>
+  );
+}
+
+export default App;
