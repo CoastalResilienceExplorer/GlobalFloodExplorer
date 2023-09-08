72f533a5916beb97bc57a0487df920dfe12fddd5
Christopher J Lowrie <chlowrie@Christophers-MacBook-Pro.local>
2023-09-08 07:30:18 -0700

    Modified build_and_deploy.sh

--- processing/dns2.sh
+++ processing/dns2.sh
@@ -1,10 +1,24 @@
+gcloud beta run domain-mappings create --service frontend-dev --domain ${ENV}.coastalresilienceexplorer.org --region us-west1
+gcloud beta run domain-mappings describe --domain ${ENV}.coastalresilienceexplorer.org --region us-west1 | yq 
+RESOURCE_NAME=$(gcloud beta run domain-mappings describe --domain ${ENV}.coastalresilienceexplorer.org --region us-west1 | yq '.status.resourceRecords[0].name')
+RESOURCE_DATA=$(gcloud beta run domain-mappings describe --domain ${ENV}.coastalresilienceexplorer.org --region us-west1 | yq '.status.resourceRecords[0].rrdata')
+RESOURCE_TYPE=$(gcloud beta run domain-mappings describe --domain ${ENV}.coastalresilienceexplorer.org --region us-west1 | yq '.status.resourceRecords[0].type')
 MANAGED_ZONE=coastalresilienceexplorer
 NAME=dev.coastalresilienceexplorer.org
 gcloud dns record-sets transaction start \
    --zone=$MANAGED_ZONE
 
-gcloud dns record-sets transaction add RR_DATA \
-   --name=$NAME \
+gcloud dns record-sets transaction remove $RESOURCE_DATA \
+   --name=${ENV}.coastalresilienceexplorer.org \
    --ttl=300 \
-   --type="CNAME" \
+   --type=$RESOURCE_TYPE \
+   --zone=$MANAGED_ZONE
+
+gcloud dns record-sets transaction add $RESOURCE_DATA \
+   --name=${ENV}.coastalresilienceexplorer.org \
+   --ttl=300 \
+   --type=$RESOURCE_TYPE \
+   --zone=$MANAGED_ZONE
+
+gcloud dns record-sets transaction execute \
    --zone=$MANAGED_ZONE
