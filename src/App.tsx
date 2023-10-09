c08100330522815e5317c8994c761b2ded8e8d6d
William Gislason <8203830+gislawill@users.noreply.github.com>
2023-10-08 23:24:54 -0700

    Adding google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import "./App.css";
 import Map from "./Map";
 import { useState } from "react";
 import { StagingLogin } from "./staging/auth";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-YTXMLHYTSE", {
+  testMode: process.env.NODE_ENV !== "production",
+});
 
 function App() {
   const [authed, setAuthed] = useState(false);
