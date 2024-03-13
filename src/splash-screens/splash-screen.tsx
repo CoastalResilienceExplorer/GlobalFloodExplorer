c72fcb9377602efde978f14b50a6a1db52cf55fb
William Gislason <8203830+gislawill@users.noreply.github.com>
2024-03-12 22:22:36 -0700

    Updating splash screen

--- src/splash-screens/splash-screen.tsx
+++ src/splash-screens/splash-screen.tsx
@@ -2,13 +2,13 @@ import ReactGA from "react-ga4";
 
 // Researchers
 import UCSC from "../assets/UCSC_White_Logo.png";
-import TNC from "../assets/TNC logo_grayscale.png";
 import IHCant from "../assets/ih_logo.png";
 
 // Other Sponsors
 import IKI from "../assets/IKI_Climate.jpg";
 import AXA from "../assets/AXA_Research_Fund.jpg";
 import WB from "../assets/world-bank-logo.jpg";
+import TNC from "../assets/Nature_Conservancy_Logo_Color.jpg";
 import { useSpring, animated } from "@react-spring/web";
 
 export const SplashScreen = ({
@@ -22,33 +22,30 @@ export const SplashScreen = ({
     opacity: showSplashScreen ? 1 : 0,
     config: { duration: 300 },
   }));
+
   const enterExplorer = async () => {
     ReactGA.event({
       category: "Splash Screen",
       action: "Enter Explorer",
     });
-    const [animation] = springPropsApi.start({ opacity: 0 });
-    animation.then(() => {
+    springPropsApi.start({ opacity: 0 })[0].then(() => {
       setSplashScreen(false);
     });
   };
+
   if (!showSplashScreen) return null;
+
   return (
     <animated.div
       style={springProps}
       className="z-10 w-full h-full flex-col flex absolute bg-cover justify-between bg-open bg-[url('./assets/SplashBackground_Mobile.jpg')] md:bg-[url('./assets/SplashBackground.jpg')]"
     >
       <header className="flex flex-row m-2 md:m-8 items-center">
-        <img src={UCSC} alt="UCSC Logo" className="w-52 max-w-[32%]" />
-        <img
-          src={TNC}
-          alt="TNC Logo"
-          className="w-60 max-w-[32%]	ml-2 md:ml-4"
-        />
+        <img src={UCSC} alt="UCSC Logo" className="w-64 mt-7 max-w-[45%]" />
         <img
           src={IHCant}
           alt="IH Logo"
-          className="w-52 max-w-[32%]	ml-2 md:ml-4"
+          className="w-64 max-w-[45%]	ml-4 md:ml-8"
         />
       </header>
       <main className="flex text-left">
@@ -71,7 +68,7 @@ export const SplashScreen = ({
             Explore the shorelines providing the most greatest benefits to
             humans and those at greatest risk in our rapidly changing climate.
           </p>
-          <p className="label-large text-white">
+          <p className="label-large text-white mb-4">
             Based on the findings of: Menéndez, P., Losada, I.J., Torres-Ortega,
             S. et al. The Global Flood Protection Benefits of Mangroves. Sci Rep
             10, 4404 (2020).{" "}
@@ -104,22 +101,32 @@ export const SplashScreen = ({
           </button>
         </div>
       </main>
-      <footer className="flex px-2 md:px-8 py-1 pr-12 md:pr-24 mr-auto bg-[length:100%_100%] bg-[url('./assets/SplashFooterBackground.png')]">
-        <img
-          src={WB}
-          alt="UCSC Logo"
-          className="max-h-12	max-w-[30%] mx-1 md:mx-4"
-        />
-        <img
-          src={AXA}
-          alt="TNC Logo"
-          className="max-h-12	max-w-[30%] mx-1 md:mx-4"
-        />
-        <img
-          src={IKI}
-          alt="IH Logo"
-          className="max-h-12 max-w-[30%] mx-1 md:mx-4"
-        />
+      <footer className="flex flex-col px-2 md:px-8 py-1 pr-12 md:pr-24 mr-auto bg-[length:100%_100%] bg-[url('./assets/SplashFooterBackground.png')]">
+        <p className="label text-left mx-1 mb-2 md:mx-4">
+          This project is made possible by the support of our sponsors:
+        </p>
+        <div className="flex">
+          <img
+            src={WB}
+            alt="The World Bank Logo"
+            className="max-h-8	max-w-[22%] md:max-w-[25%] mx-1 md:mx-4"
+          />
+          <img
+            src={AXA}
+            alt="AXA Research Fund Logo"
+            className="max-h-8	max-w-[22%] md:max-w-[25%] mx-1 md:mx-4"
+          />
+          <img
+            src={IKI}
+            alt="International Climate Initiative Logo"
+            className="max-h-8 max-w-[22%] md:max-w-[25%] mx-1 md:mx-4"
+          />
+          <img
+            src={TNC}
+            alt="TNC Logo"
+            className="max-h-8 max-w-[22%] md:max-w-[25%] mx-1 md:mx-4"
+          />
+        </div>
       </footer>
     </animated.div>
   );
