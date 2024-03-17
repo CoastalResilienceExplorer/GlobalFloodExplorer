bdec4216b440c7c5ea834081ffde49135608ed20
William Gislason <8203830+gislawill@users.noreply.github.com>
2024-03-17 15:16:27 -0700

    clean up extraClasses

--- src/compass/compass.js
+++ src/compass/compass.js
@@ -70,7 +70,7 @@ export default function Compass(props) {
         >
           <Hover
             text="Set Filter"
-            extraClasses={" " + ReversedBasemapMap[props.theme]}
+            extraClasses={ReversedBasemapMap[props.theme]}
           >
             <Icon
               icon="mdi:filter"
@@ -84,10 +84,7 @@ export default function Compass(props) {
           }
           onClick={() => adjustViewport({ bearing: 0, pitch: 0 })}
         >
-          <Hover
-            text="Reorient"
-            extraClasses={" " + ReversedBasemapMap[props.theme]}
-          >
+          <Hover text="Reorient" extraClasses={ReversedBasemapMap[props.theme]}>
             <CompassSVG
               fill={highlightCompass ? "coral" : "white"}
               className={"controls-icon compass"}
@@ -104,10 +101,7 @@ export default function Compass(props) {
           className="controls-icon-container"
           onClick={() => adjustViewport({ zoom: props.viewport.zoom + 1 })}
         >
-          <Hover
-            text="Zoom In"
-            extraClasses={" " + ReversedBasemapMap[props.theme]}
-          >
+          <Hover text="Zoom In" extraClasses={ReversedBasemapMap[props.theme]}>
             <div className="controls-icon">
               <Plus fill="white" />
             </div>
@@ -117,10 +111,7 @@ export default function Compass(props) {
           className="controls-icon-container"
           onClick={() => adjustViewport({ zoom: props.viewport.zoom - 1 })}
         >
-          <Hover
-            text="Zoom Out"
-            extraClasses={" " + ReversedBasemapMap[props.theme]}
-          >
+          <Hover text="Zoom Out" extraClasses={ReversedBasemapMap[props.theme]}>
             <div className="controls-icon">
               <Minus fill="white" />
             </div>
