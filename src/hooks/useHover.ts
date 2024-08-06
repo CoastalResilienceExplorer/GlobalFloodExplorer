3943ac341cc8a1da35a968dd878629b57af6a16e
Will Gislason <8203830+gislawill@users.noreply.github.com>
2024-08-06 11:37:31 -0700

    Simplifying tooltip hover with MapBox Popup instead of marker

--- src/hooks/useHover.ts
+++ src/hooks/useHover.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef, useCallback, useMemo } from "react";
+import { useEffect, useRef, useCallback } from "react";
 import mapboxgl from "mapbox-gl";
 import layerGroups from "layers/layers";
 import { LayerGroupName } from "types/dataModel";
@@ -6,12 +6,6 @@ import { kFormatter } from "hooks/utils/formattingUtils";
 import { BasemapStyle } from "basemap_manager/BasemapManager";
 
 const baseStyles = "transition-opacity w-56 ";
-
-interface Tessela {
-  id?: number | string;
-  properties: any;
-  lngLat: mapboxgl.LngLat;
-}
 
 interface ColorDefintion {
   bgColor: string;
@@ -44,9 +38,7 @@ export function useHover(
 ) {
   const mapScale = useRef(0);
   const timeoutRef = useRef<NodeJS.Timeout | null>(null);
-  const popover = useRef<HTMLDivElement | null>(null);
-  const [hoveredTessela, setHoveredTessela] = useState<Tessela | null>(null);
-  const hoverTimer = useRef<NodeJS.Timeout | null>(null);
+  const popover = useRef<mapboxgl.Popup | null>(null);
   const colors = ThemeMap[theme];
 
   useEffect(() => {
@@ -65,80 +57,74 @@ export function useHover(
         ]
       ) {
         map.getCanvas().style.cursor = "pointer";
-        hoverTimer.current = setTimeout(() => {
-          if (e.features?.[0]?.id && e.features[0].id !== hoveredTessela?.id) {
-            setHoveredTessela({
-              ...e.features[0],
-              lngLat: e.lngLat,
-            });
+        const hoveredTessela = e.features?.[0];
+        console.log(hoveredTessela);
+        if (popover.current) {
+          popover.current.remove();
+        }
+        popover.current = new mapboxgl.Popup({
+          closeButton: false,
+          closeOnClick: false,
+        });
+
+        popover.current.setHTML(`
+          <div class="rounded border-${
+            colors.borderColor
+          } border-[1px] p-4 bg-${colors.bgColor}">
+            <h6 class="mb-1 text-${colors.textColor}">Study Unit ${
+              hoveredTessela.id
+            } (${hoveredTessela.properties?.ISO3})</h6>
+            ${
+              layerGroups[selectedLayer]?.metricKey &&
+              hoveredTessela.properties?.[
+                layerGroups[selectedLayer]?.metricKey as string
+              ]
+                ? `<p class="lining-nums text-${
+                    colors.textColor
+                  }">${selectedLayer}:  ${
+                    selectedLayer === LayerGroupName.Population ? "" : "$"
+                  }${kFormatter(
+                    hoveredTessela.properties[
+                      layerGroups[selectedLayer]?.metricKey as string
+                    ],
+                  )} ${layerGroups[selectedLayer]?.units}</p>`
+                : ""
+            }
+          </div>
+        `);
+
+        // Copy coordinates array.
+        const coordinates = (hoveredTessela.geometry as any).coordinates as [
+          number,
+          number,
+        ];
+
+        // Populate the popup and set its coordinates
+        // based on the feature found.
+        popover.current
+          .setLngLat({
+            lng: coordinates[0],
+            lat: coordinates[1],
+          })
+          .setOffset([0, -20])
+          .addTo(map);
+
+        setTimeout(() => {
+          if (popover.current) {
+            timeoutRef.current && clearTimeout(timeoutRef.current);
+            popover.current.addClassName(baseStyles + "!opacity-100");
           }
         }, 100);
       }
     },
-    [hoveredTessela?.id, map, selectedLayer],
+    [colors.bgColor, colors.borderColor, colors.textColor, map, selectedLayer],
   );
 
   const onHoverEnd = useCallback(() => {
     map.getCanvas().style.cursor = "grab";
     setTimeout(() => {
-      setHoveredTessela(null);
-      if (hoverTimer.current) {
-        clearTimeout(hoverTimer.current);
-      }
-    }, 50);
-  }, [map]);
-
-  useEffect(() => {
-    if (hoveredTessela) {
       if (popover.current) {
-        popover.current.remove();
-      }
-      popover.current = document.createElement("div");
-      popover.current.className = baseStyles + "opacity-0";
-      popover.current.innerHTML = `
-        <div class="rounded border-${colors.borderColor} border-[1px] p-4 bg-${
-          colors.bgColor
-        }">
-          <h6 class="mb-1 text-${colors.textColor}">Study Unit ${
-            hoveredTessela.id
-          } (${hoveredTessela.properties.ISO3})</h6>
-          ${
-            layerGroups[selectedLayer]?.metricKey &&
-            hoveredTessela.properties[
-              layerGroups[selectedLayer]?.metricKey as string
-            ]
-              ? `<p class="lining-nums text-${
-                  colors.textColor
-                }">${selectedLayer}:  ${
-                  selectedLayer === LayerGroupName.Population ? "" : "$"
-                }${kFormatter(
-                  hoveredTessela.properties[
-                    layerGroups[selectedLayer]?.metricKey as string
-                  ],
-                )} ${layerGroups[selectedLayer]?.units}</p>`
-              : ""
-          }
-        </div>
-      `;
-
-      // create the marker
-      const marker = new mapboxgl.Marker(popover.current).setLngLat(
-        hoveredTessela.lngLat,
-      );
-      marker.setOffset([0, -200]);
-      marker.addTo(map);
-
-      setTimeout(() => {
-        if (popover.current) {
-          timeoutRef.current && clearTimeout(timeoutRef.current);
-          const popoverHeight = popover.current.getBoundingClientRect().height;
-          marker.setOffset([0, -popoverHeight]);
-          popover.current.className = baseStyles + "opacity-100";
-        }
-      }, 10);
-    } else {
-      if (popover.current) {
-        popover.current.className = baseStyles + "!opacity-0";
+        popover.current.addClassName(baseStyles + "!opacity-0");
         timeoutRef.current = setTimeout(() => {
           if (popover.current) {
             popover.current.remove();
@@ -146,15 +132,8 @@ export function useHover(
           }
         }, 500);
       }
-    }
-  }, [
-    colors.bgColor,
-    colors.borderColor,
-    colors.textColor,
-    hoveredTessela,
-    map,
-    selectedLayer,
-  ]);
+    }, 50);
+  }, [map]);
 
   useEffect(() => {
     map?.on("mouseenter", "tessela_rps", (e: mapboxgl.MapLayerMouseEvent) => {
